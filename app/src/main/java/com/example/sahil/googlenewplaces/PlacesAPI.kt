d842a23fd8b9517176f88dd6f7db20193d336b4c
Sahil-raja <sahilsaroj1999@gmail.com>
2019-02-04 14:21:26 +0530

    GoogleNewPlaces

--- app/src/main/java/com/example/sahil/googlenewplaces/PlacesAPI.kt
+++ app/src/main/java/com/example/sahil/googlenewplaces/PlacesAPI.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PlacesAPI {
-    @GET("maps/api/place/nearbysearch/json?key=AIzaSyDdCGdR2cnWw0AB0LeN3jOTjKmkKag4tew")
+    @GET("maps/api/place/nearbysearch/json?key=API_KEY")
 
     fun getPlaces(@Query("location") loc:String,
                   @Query("type") t:String,
